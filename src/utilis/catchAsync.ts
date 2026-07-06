5c804b2634d85019d888dae3b933cf73ed2cd4c1
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2026-07-06 21:46:52 +0600

    create higher-order function that wraps  async route handlers

---
+++ src/utilis/catchAsync.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from "express"
+import httpStatus from "http-status"
+
+
+
+export const catchAsync=(fn:RequestHanler)=>{
+    return async( req: Request, res:Response, next:NextFunction)=>{
+        try{
+             await fn(req, res, next)
+        }catch(error){
+             console.log(error)
+             const message=(error as Error).message
+             const isNotFound=/not found/i.test(message)
+             res.status(isNotFound ? httpStatus.NOT_FOUND: httpStatus.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                statusCode: isNotFound ? httpStatus.NOT_FOUND : httpStatus.INTERNAL_SERVER_ERROR,
+                message: message,
+                error: message
+             })
+
+        }
+    }
+}
