c701f111e2d2f6672e469175c23ea0d542a4aaae
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2026-07-07 00:25:36 +0600

    jwt create Token and verifytoken utils created

---
+++ src/utils/jwt.ts
@@ -0,0 +1,30 @@
+import type { JwtPayload, SignOptions } from "jsonwebtoken";
+import jwt from "jsonwebtoken"
+
+
+const createToken=(payload:JwtPayload, secret: string, options: SignOptions)=>{
+     const token= jwt.sign(payload, secret, options);
+      return token;
+}
+
+const  verifyToken=(token: string, secret: string)=>{
+       try{
+            const verifiedToken=jwt.verify(token,secret);
+            return{
+                   success: true,
+                   data: verifiedToken as JwtPayload
+            }
+
+       }catch(error: unknown){
+            const message= error instanceof Error ? error.message : "Invalid Token";
+            return {
+                   success: false,
+                   error: message
+            }
+       }
+}
+
+export const jwtUtils={
+       createToken,
+       verifyToken
+}
